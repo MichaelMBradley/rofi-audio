6967f3f3513505e8c357df02429b7e2f8b490972
Michael Bradley <michaelmbrad@gmail.com>
2023-08-25 14:20:53 -0400

    Parse pactl output

--- menu.py
+++ menu.py
@@ -5,10 +5,10 @@ import pactl
 
 class AudioMenu(rofi_menu.Menu):
     """A menu to display the available sinks, selecting one of which will set it as default."""
-    prompt = "Select Default Sink:"
+    prompt = "Select default sink:"
 
     def __init__(self, **kwargs):
         """Creates a list of available sinks."""
         super().__init__(**kwargs)
 
-        self.items = [rofi_menu.Item(pactl.list_sinks())]
+        self.items = [rofi_menu.Item(sink['name']) for sink in pactl.parse_all_sinks()]
--- pactl.py
+++ pactl.py
@@ -1,5 +1,32 @@
+import re
 import subprocess
 
 
-def list_sinks():
+def list_sinks() -> str:
     return subprocess.run(['pactl', 'list', 'sinks'], capture_output=True).stdout.decode()
+
+
+def split_sinks(all_sinks: str) -> list[str]:
+    return re.split(r'Sink #\d+', all_sinks)[1:]
+
+
+def regex_match_one(pattern: str | re.Pattern[str], string: str) -> str:
+    result = re.search(pattern, string, re.I)
+    if result and len(result.groups()) == 1:
+        return result.groups()[0]
+
+
+def get_sink_attribute(sink: str, attr: str) -> str | None:
+    return regex_match_one(rf'\n\s+{attr}: ([^\n]+)', sink)
+
+
+def get_sink_property(sink: str, prop: str) -> str | None:
+    return regex_match_one(rf'\n\s+{prop} = "([^\n]+)"\n', sink)
+
+
+def parse_sink(sink: str) -> dict[str, str | None]:
+    return {attr: get_sink_attribute(sink, attr) for attr in ['name', 'description']}
+
+
+def parse_all_sinks() -> list[dict[str, str | None]]:
+    return list(map(parse_sink, split_sinks(list_sinks())))
